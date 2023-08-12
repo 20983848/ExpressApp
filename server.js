8faf51502dc1fe998953733d724bd4c264a886f1
Ryan <20983848@student.curtin.edu.au>
2023-08-13 01:02:36 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var file = require("fs");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+const router = express.Router();
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname + "/" + "main.html");
+   
+});
+
+
+
+app.get("/signup", function(req,res){
+
+    var packet = {
+        name: req.query.name
+        
+    }
+    
+    console.log(packet.name);
+    
+    res.end();
+});
+
+
+var server = app.listen(process.env.port, function(){
+
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(host,port);
+
+});
+
+module.exports = router;
+
+
+
